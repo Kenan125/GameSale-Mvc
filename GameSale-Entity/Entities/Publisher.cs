ec0e63b72d9767011f3092f2e90b166df6c48ef6
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2024-08-11 04:24:28 +0300

    fix

--- GameSale-Entity/Entities/Publisher.cs
+++ GameSale-Entity/Entities/Publisher.cs
@@ -2,7 +2,7 @@
 {
     public class Publisher : BaseEntity
 	{
-        public int Id { get; set; }
+        
 
         public string Name { get; set; }
 
--- GameSale-Service/Mapping/MappingProfile.cs
+++ GameSale-Service/Mapping/MappingProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using GameSale_Entity.Entities;
+using GameSale_Entity.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,22 @@ using System.Threading.Tasks;
 
 namespace GameSale_Service.Mapping
 {
-	public class MappingProfile
+	public class MappingProfile : Profile
 	{
+		public MappingProfile() 
+		{ 
+			CreateMap<Game, GameViewModel>().ReverseMap(); 
+			CreateMap<Category, CategoryViewModel>().ReverseMap(); 
+			CreateMap<Customer, CustomerViewModel>().ReverseMap(); 
+			CreateMap<GameSale, GameSaleViewModel>().ReverseMap(); 
+			CreateMap<GameSaleDetail, GameSaleDetailViewModel>().ReverseMap();
+			CreateMap<Image, ImageViewModel>().ReverseMap();
+			CreateMap<Publisher, PublisherViewModel>().ReverseMap();
+            CreateMap<Review, ReviewViewModel>().ReverseMap();
+            CreateMap<SystemRequirement, SystemRequirementViewModel>().ReverseMap();
 
+
+        }
+		
 	}
 }
